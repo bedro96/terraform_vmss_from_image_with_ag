f34eb9d4e364676a6b45d4e93a33f3614d018181
bedro96@naver.com <bedro96@naver.com>
2019-12-24 08:46:46 +0000

    update on shell

--- setup.sh
+++ setup.sh
@@ -26,14 +26,14 @@ crontab crons.sh
 
 #################### datadisk_setup.sh ##################
 # fdisk /dev/sdc
-echo "n\np\n\n\n\nw\n" | fdisk /dev/sdc
+sudo sh -c 'echo "n\np\n\n\n\nw\n" | fdisk /dev/sdc'
 # mkfs
-mkfs -t ext4 /dev/sdc1
+sudo mkfs -t ext4 /dev/sdc1
 # create a mount point
-mkdir -p /logs
-chmod 777 /logs
+sudo mkdir -p /logs
+sudo chmod 777 /logs
 # mount
-mount /dev/sdc1 /logs
+sudo mount /dev/sdc1 /logs
 # Get UUID for /dev/sdc and place in fstab
 # sudo -i blkid
 # /dev/sdc1: UUID="33333333-3b3b-3c3c-3d3d-3e3e3e3e3e3e" TYPE="ext4"
@@ -42,5 +42,5 @@ sdc_uuid=$(sudo -i blkid | grep ^/dev/sdc1 | awk -F '"' '{print $2}')
 # UUID=33333333-3b3b-3c3c-3d3d-3e3e3e3e3e3e   /logs   ext4   defaults   1   2
 inputstring="UUID=$sdc_uuid /logs ext4 defaults 1 2"
 echo $inputstring | sudo tee -a /etc/fstab
-umount /logs
-mount /logs
+sudo umount /logs
+sudo mount /logs
